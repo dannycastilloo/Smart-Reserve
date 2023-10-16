f837f5786b94795b2ac3dd2aa7712f9df3b1f1ca
dannycastilloo <dannycastillootiniano@gmail.com>
2023-10-16 16:58:49 -0500

    vulnerabilidad de url corregida

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { ReserveScreen } from './routes/ReserveScreen';
 import { StatsScreen } from './routes/StatsScreen';
@@ -6,36 +6,43 @@ import { ProfileScreen } from './routes/ProfileScreen';
 import { HomeScreen } from './routes/HomeScreen';
 import LoginScreen from './routes/LoginScreen';
 import NavBar from './components/NavBar';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      setUser(authUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/reserve" element={<ReserveScreen />} />
-        <Route path="/stats" element={<StatsScreen />} />
-        <Route path="/profile" element={<ProfileScreen />} />
         <Route path="/login" element={<LoginScreen />} />
-        <Route path="/home/*" element={<ProtectedRoutes />} />
-        <Route path="/*" element={<Navigate to="/login" />} />
+        {user && (
+          <Route
+            path="/app"
+            element={
+              <>
+                <NavBar />
+                <Routes>
+                  <Route path="/" element={<Navigate to="/home" />} />
+                  <Route path="/reserve" element={<ReserveScreen />} />
+                  <Route path="/stats" element={<StatsScreen />} />
+                  <Route path="/profile" element={<ProfileScreen />} />
+                  <Route path="/home" element={<HomeScreen />} />
+                </Routes>
+              </>
+            }
+          />
+        )}
       </Routes>
     </>
   );
-};
-
-const ProtectedRoutes = () => {
-  const isAuthenticated = true;
-
-  if (isAuthenticated) {
-    return (
-      <>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
-      </>
-    );
-  } else {
-    return <Navigate to="/login" />;
-  }
 };
